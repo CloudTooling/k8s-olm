1c14750fc0f6c78f33a9288568608df56253654c
Martin Reinhardt <contact@martinreinhardt-online.de>
2023-07-14 06:20:47 +0200

    fix(Update): Correcting update shell script

--- .bin/update-chart.sh
+++ .bin/update-chart.sh
@@ -3,7 +3,7 @@
 set -o pipefail
 
 # renovate: datasource=github-tags depName=operator-framework/operator-lifecycle-manager versioning=semver
-OLM_VERSION="v0.24.0"
+OLM_VERSION="v0.25.0"
 
 mkdir -p tmp && cd tmp
 git clone https://github.com/operator-framework/operator-lifecycle-manager.git
@@ -11,6 +11,7 @@ git -C operator-lifecycle-manager reset --hard "$OLM_VERSION"
 echo "Using OLM version=$OLM_VERSION"
 sed -i.bak "s/^[Vv]ersion:.*\$/version: ${OLM_VERSION}/" operator-lifecycle-manager/deploy/chart/Chart.yaml
 rm operator-lifecycle-manager/deploy/chart/Chart.yaml.bak
+rm -rf ../chart
 mv operator-lifecycle-manager/deploy/chart ..
 cd ..
 rm -rf tmp
