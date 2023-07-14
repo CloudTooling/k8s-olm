5097d76996cb37149e2f8d7c70531a36b5955c89
Martin Reinhardt <contact@martinreinhardt-online.de>
2023-07-14 06:22:19 +0200

    fix(Update): Rollback version

--- .bin/update-chart.sh
+++ .bin/update-chart.sh
@@ -3,11 +3,11 @@
 set -o pipefail
 
 # renovate: datasource=github-tags depName=operator-framework/operator-lifecycle-manager versioning=semver
-OLM_VERSION="v0.25.0"
+OLM_VERSION="0.24.0"
 
 mkdir -p tmp && cd tmp
 git clone https://github.com/operator-framework/operator-lifecycle-manager.git
-git -C operator-lifecycle-manager reset --hard "$OLM_VERSION"
+git -C operator-lifecycle-manager reset --hard "v$OLM_VERSION"
 echo "Using OLM version=$OLM_VERSION"
 sed -i.bak "s/^[Vv]ersion:.*\$/version: ${OLM_VERSION}/" operator-lifecycle-manager/deploy/chart/Chart.yaml
 rm operator-lifecycle-manager/deploy/chart/Chart.yaml.bak
