35c2b7db3b5d52fe2214d998eae37734d85239f3
Martin Reinhardt <contact@martinreinhardt-online.de>
2023-11-01 05:10:35 +0100

    chore: Fix Chart.yaml exclude

--- .bin/update-chart.sh
+++ .bin/update-chart.sh
@@ -6,13 +6,14 @@ set -o pipefail
 OLM_VERSION="0.26.0"
 
 mkdir -p tmp && cd tmp
+cp ../chart/Chart.yaml Chart.yaml.bak
 git clone https://github.com/operator-framework/operator-lifecycle-manager.git
 git -C operator-lifecycle-manager reset --hard "v$OLM_VERSION"
 echo "Using OLM version=$OLM_VERSION"
-sed -i.bak "s/^[Vv]ersion:.*\$/version: ${OLM_VERSION}/" operator-lifecycle-manager/deploy/chart/Chart.yaml
-rm operator-lifecycle-manager/deploy/chart/Chart.yaml.bak
+rm  operator-lifecycle-manager/deploy/chart/Chart.yaml
 rm -rf ../chart
 mv operator-lifecycle-manager/deploy/chart ..
+mv Chart.yaml.bak ../chart/Chart.yaml
 cd ..
 sed -i.bak "s/operator-framework\/olm:master$/operator-framework\/olm:v${OLM_VERSION}/" chart/values.yaml
 rm chart/values.yaml.bak
