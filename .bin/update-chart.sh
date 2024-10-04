ccac8fe67581878b117690215eb6f81f3538f635
Martin Reinhardt <contact@martinreinhardt-online.de>
2024-10-04 08:09:25 +0200

    chore: Adding end2end testing for chart

--- .bin/update-chart.sh
+++ .bin/update-chart.sh
@@ -7,6 +7,7 @@ OLM_VERSION="0.28.0"
 
 mkdir -p tmp && cd tmp
 cp ../chart/Chart.yaml Chart.yaml.bak
+cp ../chart/ci.values.yaml ci.values.yaml.bak
 git clone https://github.com/operator-framework/operator-lifecycle-manager.git
 git -C operator-lifecycle-manager reset --hard "v$OLM_VERSION"
 echo "Using OLM version=$OLM_VERSION"
@@ -14,6 +15,7 @@ rm  operator-lifecycle-manager/deploy/chart/Chart.yaml
 rm -rf ../chart
 mv operator-lifecycle-manager/deploy/chart ..
 mv Chart.yaml.bak ../chart/Chart.yaml
+mv ci.values.yaml.bak ../chart/ci.values.yaml.yaml
 cd ..
 sed -i.bak "s/operator-framework\/olm:master$/operator-framework\/olm:v${OLM_VERSION}/" chart/values.yaml
 rm chart/values.yaml.bak
